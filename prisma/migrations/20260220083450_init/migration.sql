aa5e4dccc2c2642014292a7465bc9dd010800fcc
Miraj  Hossen <mirajhossen@Softvences-Mac-mini-4.local>
2026-02-25 13:54:05 +0600

    Remove foloder

--- prisma/migrations/20260220083450_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "lastViewed" TIMESTAMP(3),
-ADD COLUMN     "profileViews" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20260224105906_adding_highlights_notification/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Notification" ADD COLUMN     "highlightId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Notification" ADD CONSTRAINT "Notification_highlightId_fkey" FOREIGN KEY ("highlightId") REFERENCES "highlights"("id") ON DELETE CASCADE ON UPDATE CASCADE;
