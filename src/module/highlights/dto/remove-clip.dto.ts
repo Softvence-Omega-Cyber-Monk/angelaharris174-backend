3051d2e9f323b594b51379c38b4fd834715b9319
Miraj  Hossen <mirajhossen@Softvences-Mac-mini-4.local>
2026-02-27 17:22:06 +0600

    Updated issue

--- src/module/highlights/dto/remove-clip.dto.ts
+++ src/module/highlights/dto/remove-clip.dto.ts
@@ -2,17 +2,17 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class RemoveClipDto {
-    @ApiProperty({
-        example: 'cmlc3mqvf0000rcwa6ajj9w24',
-        description: 'ID of the highlight to modify',
-    })
-    @IsString()
-    highlightId: string;
+  @ApiProperty({
+    example: 'cmlc3mqvf0000rcwa6ajj9w24',
+    description: 'ID of the highlight to modify',
+  })
+  @IsString()
+  highlightId: string;
 
-    @ApiProperty({
-        example: 1,
-        description: 'The order index of the clip to remove',
-    })
-    @IsNumber()
-    order: number;
+  @ApiProperty({
+    example: 1,
+    description: 'The order index of the clip to remove',
+  })
+  @IsNumber()
+  order: number;
 }
